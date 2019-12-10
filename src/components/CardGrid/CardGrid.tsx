16107a92f23e50f16ceab10be6197dc3971112ce
Katarzyna Tobis <tobis.katarzyna@gmail.com>
2019-12-10 19:19:20 +0100

    mockup data

--- src/components/CardGrid/CardGrid.tsx
+++ src/components/CardGrid/CardGrid.tsx
@@ -5,6 +5,8 @@ import { Actor, FetchedMovie, MovieWithActorNames } from "../../types";
 import Loader from "../Loader/Loader";
 import Card from "./Card/Card";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import * as moviesFile from "../../assets/movies.json";
+import * as actorsFile from "../../assets/actors.json";
 
 const CardGrid: React.FC = () => {
   const [movies, setMovies] = useState<Array<MovieWithActorNames> | undefined>(
@@ -14,16 +16,19 @@ const CardGrid: React.FC = () => {
   const [error, setError] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    const moviesPromise = fetchData<Array<FetchedMovie>>(
-      "https://0zrzc6qbtj.execute-api.us-east-1.amazonaws.com/kinside/movies"
-    );
-    const actorsPromise = fetchData<Array<Actor>>(
-      "https://0zrzc6qbtj.execute-api.us-east-1.amazonaws.com/kinside/actors"
-    );
+    // const moviesPromise = fetchData<Array<FetchedMovie>>(
+    //   "https://0zrzc6qbtj.execute-api.us-east-1.amazonaws.com/kinside/movies"
+    // );
+    // const actorsPromise = fetchData<Array<Actor>>(
+    //   "https://0zrzc6qbtj.execute-api.us-east-1.amazonaws.com/kinside/actors"
+    // );
+
+    const moviesPromise = Promise.resolve(JSON.parse(JSON.stringify((moviesFile as any).default)));
+    const actorsPromise = Promise.resolve(JSON.parse(JSON.stringify((actorsFile as any).default)));
 
     Promise.all([moviesPromise, actorsPromise])
       .then(([movies, actors]) => {
-        const moviesWithActors = movies.map(movie => {
+        const moviesWithActors = movies.map((movie: FetchedMovie) => {
           const actorNames = movie.actorIds.map(id => getActorName(id, actors));
           return { ...movie, actors: actorNames };
         });
