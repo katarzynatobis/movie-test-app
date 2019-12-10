7249558bf15d3031faf7bc97e62e7c6dd43ca75e
Katarzyna Tobis <tobis.katarzyna@gmail.com>
2019-12-11 00:04:25 +0100

    Updates

---
+++ precache-manifest.93a89420a1a59f9b50da0e8fbc070515.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281e67f2550340d5f28325400cd2b9c2",
+    "url": "/movie-test-app/index.html"
+  },
+  {
+    "revision": "915c39916bdfc8efeb8f",
+    "url": "/movie-test-app/static/css/main.19280d4a.chunk.css"
+  },
+  {
+    "revision": "ac96f80c995e5b602641",
+    "url": "/movie-test-app/static/js/2.1306f090.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie-test-app/static/js/2.1306f090.chunk.js.LICENSE"
+  },
+  {
+    "revision": "915c39916bdfc8efeb8f",
+    "url": "/movie-test-app/static/js/main.34bbfc91.chunk.js"
+  },
+  {
+    "revision": "6c5c379b93aaeddfddf2",
+    "url": "/movie-test-app/static/js/runtime-main.80df288b.js"
+  },
+  {
+    "revision": "6873bb5f5c776cbb1ef1cd3b6c782feb",
+    "url": "/movie-test-app/static/media/images-solid.6873bb5f.svg"
+  },
+  {
+    "revision": "46a261ba73692a2a0d1ce0218d7a74e1",
+    "url": "/movie-test-app/static/media/pug.46a261ba.jpg"
+  }
+]);
